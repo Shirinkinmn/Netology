e6c5fa4cf071cab3bd4ca4a8ae5a02764a89fa8a
Shirinkinmn <47269199+Shirinkinmn@users.noreply.github.com>
2019-03-22 17:42:32 +0500

    Update main.py

--- Бухгалтерия/main.py
+++ Бухгалтерия/main.py
@@ -1,5 +1,6 @@
 from application.salary import calculate_salary
 from application.db.people import get_employees
 
-calculate_salary()
-get_employees()
+if __name__ == "__main__":
+    calculate_salary()
+    get_employees()
